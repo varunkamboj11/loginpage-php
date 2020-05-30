6d3c562a5b4eac2a4aad1c5b42dd19ce110305fc
varunkamboj11 <kambojvarun1111@gmail.com>
2020-05-31 01:08:20 +0530

    updateusername

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
 	
    <body>
       
-      <h2>Enter Username and Password</h2> 
+      <h2>Enter Username1 and Password</h2> 
       <div class = "container form-signin">
          
          <?php
